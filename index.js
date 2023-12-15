2564e9d70c62cb13092d9e4dbe890bb3dbc5058c
Nazeh Abel <talentednazeh@gmail.com>
2023-12-15 14:51:22 +0000

    chore: Update vercel endpoints

--- index.js
+++ index.js
@@ -10,36 +10,34 @@ const cors = require("cors");
 const app = express();
 const allRoutes = require("./routes/api");
 const handleErrors = require("./middlewares/HandleException");
-const { RequestMethods } = require("./middlewares/acceptRequestMethods");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
-// If you have more origins you would like to add, you can add them to the array below.
 const allowedOrigins = [
   "http://localhost:3000",
   "https://rightbox.herokuapp.com",
-  "https://rightbox.herokuapp.com/",
   "https://rightbox-web-bgjx.vercel.app",
-  "https://rightbox-web-bgjx.vercel.app/",
-  "https://rightbox-web-bgjx-l3z7d04xg-nazehs.vercel.app/",
   "https://rightbox-web-bgjx-l3z7d04xg-nazehs.vercel.app",
   "https://rightbox-web-bgjx-8bwox6jqs-nazehs.vercel.app",
-  "https://rightbox-web-bgjx-8bwox6jqs-nazehs.vercel.app/"
 ];
 
-const options = {
-  origin: allowedOrigins,
-  optionSuccessStatus: 200,
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
 };
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+
+app.use(cors(corsOptions));
+
 app.use("/api", allRoutes);
-app.use((err, req, res, next) => {
-  res
-    .status(err.statusCode)
-    .send({ statusCode: err.statusCode, status: 1, message: err.message });
-});
+
 app.use("/cookies", (req, res) => {
   const dataToSecure = {
     dataToSecure: "This is the secret data in the cookie.",
@@ -48,23 +46,23 @@ app.use("/cookies", (req, res) => {
   res.cookie("secureCookie", JSON.stringify(dataToSecure), {
     secure: process.env.NODE_ENV !== "development",
     httpOnly: true,
-    expires: dayjs()
-      .add(30, "days")
-      .toDate(),
+    expires: dayjs().add(30, "days").toDate(),
   });
 
   res.send("Hello.");
 });
+
 // GET
 
-// handling for wrong endpoint get request
+// Handling for wrong endpoint get request
 app.get("*", (req, res, next) => {
   // Error goes via `next()` method
   setImmediate(() => {
     next(new Error("Oops, Something went wrong. Kindly check your endpoint"));
   });
 });
-// handling for wrong endpoint post request
+
+// Handling for wrong endpoint post request
 app.post("*", (req, res, next) => {
   // Error goes via `next()` method
   setImmediate(() => {
@@ -72,18 +70,20 @@ app.post("*", (req, res, next) => {
   });
 });
 
-// setting an environment  || port number
+// Error handling middleware
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    status: err.statusCode || 500,
+    message: err.message || "Internal Server Error",
+  });
+});
+
+// Setting the environment || port number
 const PORT = process.env.PORT || config.PORT;
-// middlewares
-app.use(handleErrors);
 
-// This will manage our sessions
-// app.use(sessionHandler)
-app.use(RequestMethods);
-
-// telling our application which port to listen from.
-app.listen(PORT, (req, res, next) => {
-  console.log(`${config.Envoirnment} server is running at port ${PORT}`);
+// Telling our application which port to listen to.
+app.listen(PORT, () => {
+  console.log(`${config.Environment} server is running at port ${PORT}`);
 });
 
 module.exports = app;
